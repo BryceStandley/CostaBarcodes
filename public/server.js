84ccad4cf22efce5ce2b5ffe29dce78512ff0cb4
Bryce Standley <bryce@vectorpixel.net>
2022-11-28 20:12:32 +0800

    1.2.2
    -Fixed minor routing issue with express.js
    -Added release action
    -Fixed scss file loading

--- public/server.js
+++ public/server.js
@@ -5,14 +5,24 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'build', 'static')));
 
 app.get('/static/*', function (req, res) {
+  console.log('Sending: {0}', req.originalUrl)
+  res.sendFile(path.join(__dirname, req.originalUrl));
+  
+});
+
+app.get('/assets/*', function (req, res) {
+  console.log('Sending: {0}', req.originalUrl)
   res.sendFile(path.join(__dirname, req.originalUrl));
 });
 
 app.get('/', function (req, res) {
+  console.log('Sending: {0}', req.originalUrl)
+
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('*', function (req, res) {
+    console.log('Redirect to / from request: {0}', req.originalUrl)
     res.redirect('/');
 });
 
